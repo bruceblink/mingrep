8ce0255995b9cd9ed685b47739f0fe2425a370ad
likanug <likanug.g@qq.com>
2025-06-28 16:27:56 +0800

    refactor: use parse_config function parse CLI args

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     // 获取命令的2个参数
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = parse_config(&args);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.1)
     .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
+}
+
+
+// 解析命令行参数
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
