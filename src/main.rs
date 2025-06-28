4a265a03043c36eac0406c741b31c3afc7a070ed
likanug <likanug.g@qq.com>
2025-06-28 13:55:56 +0800

    添加获取命令行参数的功能

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    // 获取命令的2个参数
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
