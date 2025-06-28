25543fc25deab1c6c6f8e08faade667fc4580fe9
likanug <likanug.g@qq.com>
2025-06-28 16:38:48 +0800

    refactor: use Config struct construct command line args

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,27 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     // 获取命令的2个参数
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
-    let contents = fs::read_to_string(config.1)
+    let contents = fs::read_to_string(config.file_path)
     .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 }
 
+// 定义配置结构体
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-// 解析命令行参数
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+impl Config {
+    // 创建一个新的配置实例
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config {
+            query,
+            file_path
+        }
+    }
 }
